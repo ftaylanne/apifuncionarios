3efee7822a0a2986cf4b303ac4f6691b047a75c0
ftaylanne <taay.camyli@gmail.com>
2022-04-30 01:27:33 +0100

    first commit

---
+++ ApiFuncionarios.Infra.Data/Entities/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiFuncionarios.Infra.Data.Entities
+{
+    public class Funcionario
+    {
+        public Guid IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataAdmissao { get; set; }
+    }
+}
